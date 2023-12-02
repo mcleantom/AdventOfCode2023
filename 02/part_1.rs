2c80ec7580fb9c4011291bb1888c5bbc12851bd3
Tom McLean <mcleantom97@gmail.com>
2023-12-02 22:06:32 +0000

    part 1 complete

--- 02/part_1.rs
+++ 02/part_1.rs
@@ -10,35 +10,29 @@ fn is_valid_game(line: &String) -> bool {
         ("blue", 14)
     ]);
 
-    match line.split_once(": ") {
-        Some((_, games)) => {
-            for sub_game in games.split("; ") {
-                for chosen_color in sub_game.split(", ") {
-                    match chosen_color.split_once(" ") {
-                        Some((n_balls, color)) => {
-                            match color_to_max_num_balls.get(&color) {
-                                Some(max_n_balls) => {
-                                    if n_balls.parse::<i32>().unwrap() > *max_n_balls {
-                                        println!("{}, {}", color, n_balls);
-                                        return false
-                                    }
-                                },
-                                None => {
-                                    panic!("Color was not one of green, red or blue");
-                                }
+    for sub_game in line.split_once(": ").unwrap() {
+        for chosen_color in sub_game.split(", ").unwrap() {
+            match chosen_color.split_once(" ") {
+                Some((n_balls, color)) => {
+                    match color_to_max_num_balls.get(&color) {
+                        Some(max_n_balls) => {
+                            if n_balls.parse::<i32>().unwrap() > *max_n_balls {
+                                println!("{}, {}", color, n_balls);
+                                return false
                             }
                         },
                         None => {
-                            panic!("Was expecting the string to split games with a ', ': {}", line);
+                            panic!("Color was not one of green, red or blue");
                         }
                     }
+                },
+                None => {
+                    panic!("Was expecting the string to split games with a ', ': {}", line);
                 }
             }
-        },
-        None => {
-            panic!("Was expecting the string to start with game n: {}", line);
         }
     }
+
     true
 }
 
