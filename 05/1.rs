05be0c1bc715a9fe58e0c10001408a37abb5653a
Tom McLean <mcleantom97@gmail.com>
2023-12-05 13:43:42 +0000

    faster

--- 05/1.rs
+++ 05/1.rs
@@ -1,33 +1,85 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::collections::HashMap;
+
+
+#[derive(Debug)]
+struct MapRange {
+    source: i64,
+    destination: i64,
+    range: i64
+}
+
+impl MapRange {
+    fn new(source: i64, destination: i64, range: i64) -> MapRange {
+        MapRange {
+            source,
+            destination,
+            range
+        }
+    }
+
+    fn get(&self, x: i64) -> Option<i64> {
+        if x >= self.source && x <= self.source + self.range {
+            let diff = x - self.source;
+            return Some(self.destination + diff);
+        }
+        None
+    }
+}
+
+
+#[derive(Debug)]
+struct MapRanges {
+    ranges: Vec<MapRange>
+}
+
+impl MapRanges {
+    fn new() -> MapRanges {
+        MapRanges {
+            ranges: vec![]
+        }
+    }
+
+    fn get(&self, x: i64) -> i64 {
+        for range in self.ranges.iter() {
+            let v = range.get(x);
+            match v {
+                Some(k) => return k,
+                None => {}
+            }
+        }
+        x
+    }
+}
+
+
 
 #[derive(Debug)]
 struct Maps {
     seeds: Vec<i64>,
-    seed_to_soil: HashMap<i64, i64>,
-    soil_to_fertilizer: HashMap<i64, i64>,
-    fertilizer_to_water: HashMap<i64, i64>,
-    water_to_light: HashMap<i64, i64>,
-    light_to_temperature: HashMap<i64, i64>,
-    temperature_to_humidity: HashMap<i64, i64>,
-    humidity_to_location: HashMap<i64, i64>,
+    seed_to_soil: MapRanges,
+    soil_to_fertilizer: MapRanges,
+    fertilizer_to_water: MapRanges,
+    water_to_light: MapRanges,
+    light_to_temperature: MapRanges,
+    temperature_to_humidity: MapRanges,
+    humidity_to_location: MapRanges,
 }
 
 impl Maps {
     fn new(lines: &Vec<String>) -> Maps {
         let mut maps = Maps {
             seeds: vec![],
-            seed_to_soil: HashMap::new(),
-            soil_to_fertilizer: HashMap::new(),
-            fertilizer_to_water: HashMap::new(),
-            water_to_light: HashMap::new(),
-            light_to_temperature: HashMap::new(),
-            temperature_to_humidity: HashMap::new(),
-            humidity_to_location: HashMap::new(),
+            seed_to_soil: MapRanges::new(),
+            soil_to_fertilizer: MapRanges::new(),
+            fertilizer_to_water: MapRanges::new(),
+            water_to_light: MapRanges::new(),
+            light_to_temperature: MapRanges::new(),
+            temperature_to_humidity: MapRanges::new(),
+            humidity_to_location: MapRanges::new(),
         };
 
-        let mut current_hashmap: Option<&mut HashMap<i64, i64>> = None;
+        let mut current_hashmap: Option<&mut MapRanges> = None;
 
         for line in lines {
             let words: Vec<&str> = line.split_whitespace().collect();
@@ -63,13 +115,7 @@ impl Maps {
                     let destination_range_start = values[0];
                     let source_range_start = values[1];
                     let range_length = values[2];
-                    for i in 0..range_length {
-                        let source = source_range_start + i;
-                        let destination = destination_range_start + i;
-                        if let Some(ref mut map) = current_hashmap {
-                            map.insert(source, destination);
-                        }
-                    }
+                    current_hashmap.as_mut().unwrap().ranges.push(MapRange::new(source_range_start, destination_range_start, range_length));
                 }
             }
         }
@@ -78,13 +124,13 @@ impl Maps {
     }
 
     fn get_location(&self, seed: i64) -> i64 {
-        let soil = self.seed_to_soil.get(&seed).cloned().unwrap_or(seed);
-        let fertilizer = self.soil_to_fertilizer.get(&soil).cloned().unwrap_or(soil);
-        let water = self.fertilizer_to_water.get(&fertilizer).cloned().unwrap_or(fertilizer);
-        let light = self.water_to_light.get(&water).cloned().unwrap_or(water);
-        let temperature = self.light_to_temperature.get(&light).cloned().unwrap_or(light);
-        let humidity = self.temperature_to_humidity.get(&temperature).cloned().unwrap_or(temperature);
-        let location = self.humidity_to_location.get(&humidity).cloned().unwrap_or(humidity);
+        let soil = self.seed_to_soil.get(seed);
+        let fertilizer = self.soil_to_fertilizer.get(soil);
+        let water = self.fertilizer_to_water.get(fertilizer);
+        let light = self.water_to_light.get(water);
+        let temperature = self.light_to_temperature.get(light);
+        let humidity = self.temperature_to_humidity.get(temperature);
+        let location = self.humidity_to_location.get(humidity);
         location
     }
 }
