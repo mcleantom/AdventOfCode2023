09edd6b91bffb9d9c20e8b117629f4dc4da205e8
Tom McLean <mcleantom97@gmail.com>
2023-12-06 11:22:15 +0000

    optimization working

--- src/bin.rs
+++ src/bin.rs
@@ -25,27 +25,27 @@ fn main() {
     // assert_eq!(day1::part2(INPUT_D1), 54875);
 
     assert_eq!(day2::part1(INPUT_D2_TEST), 8);
-    assert_eq!(day2::part1(INPUT_D2), 2207);
-    assert_eq!(day2::part2(INPUT_D2_TEST), 2286);
-    assert_eq!(day2::part2(INPUT_D2), 62241);
+    assert_eq!(day2::part1(INPUT_D2), 2_207);
+    assert_eq!(day2::part2(INPUT_D2_TEST), 2_286);
+    assert_eq!(day2::part2(INPUT_D2), 622_41);
 
-    assert_eq!(day3::part1(INPUT_D3_TEST), 4361);
-    assert_eq!(day3::part1(INPUT_D3), 532331);
-    assert_eq!(day3::part2(INPUT_D3_TEST), 467835);
-    assert_eq!(day3::part2(INPUT_D3), 82301120);
+    assert_eq!(day3::part1(INPUT_D3_TEST), 4_361);
+    assert_eq!(day3::part1(INPUT_D3), 532_331);
+    assert_eq!(day3::part2(INPUT_D3_TEST), 467_835);
+    assert_eq!(day3::part2(INPUT_D3), 82_301_120);
 
     assert_eq!(day4::part1(INPUT_D4_TEST), 13);
-    assert_eq!(day4::part1(INPUT_D4), 21485);
+    assert_eq!(day4::part1(INPUT_D4), 21_485);
     assert_eq!(day4::part2(INPUT_D4_TEST), 30);
-    assert_eq!(day4::part2(INPUT_D4), 11024379);
+    assert_eq!(day4::part2(INPUT_D4), 11_024_379);
 
     assert_eq!(day5::part1(INPUT_D5_TEST), 35);
-    assert_eq!(day5::part1(INPUT_D5), 178159714);
+    assert_eq!(day5::part1(INPUT_D5), 178_159_714);
     assert_eq!(day5::part2(INPUT_D5_TEST), 46);
-    assert_eq!(day5::part2(INPUT_D5), 100165128);
+    assert_eq!(day5::part2(INPUT_D5), 100_165_128);
 
     assert_eq!(day6::part1(INPUT_D6_TEST), 288);
-    assert_eq!(day6::part1(INPUT_D6), 440000);
-    assert_eq!(day6::part2(INPUT_D6_TEST), 71503);
-    assert_eq!(day6::part2(INPUT_D6), 313007);
+    assert_eq!(day6::part1(INPUT_D6), 440_000);
+    assert_eq!(day6::part2(INPUT_D6_TEST), 71_503);
+    assert_eq!(day6::part2(INPUT_D6), 26_187_338);
 }
--- src/days/day6.rs
+++ src/days/day6.rs
@@ -5,27 +5,41 @@ struct Race {
 }
 
 
-fn race_distance(waiting_time: u64, race_time: &u64) -> u64 {
-    let speed = waiting_time;
-    let remaining_time = race_time - waiting_time;
-    let distance = speed * remaining_time;
-    distance
+fn number_of_ways_to_beat_race_distance(t_run: u32, dist_record: i64, v:u8) -> i64 {
+    /*
+    The inequality is:
+    distance = wait_time * (race_time - wait_time)
+    d = w * (t - w)
+    d = wt - w^2
+    w^2 - wt + d = 0
+
+    a = 1
+    b = -t
+    c = d
+
+    w = (-b +- sqrt(b^2 - 4ac)) / 2a
+    */
+    let a: i64 = -1;
+    let b: i64 = i64::from(v as u32 * t_run);
+    let c: i64 = -dist_record;
+
+    let b_squared = b.pow(2);
+    let term = b_squared - 4 * a * c;
+    let sq = f64::sqrt(term as f64);
+
+    let a_doubled = 2 * a;
+
+    let x_1 = (-b as f64 + sq) / a_doubled as f64;
+    let x_2 = (-b as f64 - sq) / a_doubled as f64;
+
+    let (x_1, x_2) = (x_1.floor() as i64 + 1, x_2.ceil() as i64 - 1);
+
+    let (a, b) = (x_1, x_2);
+    b - a + 1
 }
 
 
-fn number_of_ways_to_beat_race_distance(race: &Race) -> u64 {
-    let mut ways = 0;
-    for waiting_time in 0..race.time {
-        let distance = race_distance(waiting_time, &race.time);
-        if distance > race.distance {
-            ways += 1;
-        }
-    }
-    ways
-}
-
-
-pub fn part1(lines: &str) -> u64 {
+pub fn part1(lines: &str) -> i64 {
     let (time_str, distance_str) = lines.split_once("\n").unwrap();
 
     let times = time_str
@@ -54,9 +68,9 @@ pub fn part1(lines: &str) -> u64 {
         })
         .collect();
 
-    let number_of_ways: Vec<u64> = races
+    let number_of_ways: Vec<i64> = races
         .iter()
-        .map(|race| number_of_ways_to_beat_race_distance(&race))
+        .map(|race| number_of_ways_to_beat_race_distance(race.time as u32, race.distance as i64, 1))
         .collect();
     
     let product = number_of_ways
@@ -67,7 +81,7 @@ pub fn part1(lines: &str) -> u64 {
 }
 
 
-pub fn part2(lines: &str) -> u64 {
+pub fn part2(lines: &str) -> i64 {
     let (time_str, distance_str) = lines.split_once("\n").unwrap();
     let time = time_str
         .strip_prefix("Time: ")
@@ -89,6 +103,6 @@ pub fn part2(lines: &str) -> u64 {
         time,
         distance,
     };
-    let number_of_ways = number_of_ways_to_beat_race_distance(&race);
+    let number_of_ways = number_of_ways_to_beat_race_distance(race.time as u32, race.distance as i64, 1);
     number_of_ways
 }
